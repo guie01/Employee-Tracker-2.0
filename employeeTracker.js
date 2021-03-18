dedd13e52cf9fb35b9076461929f6f57077e6e76
Guillermo Garcia <ggq0107@hotmail.com>
2021-03-18 11:21:31 -0400

    able to display all employees, dept and roles after choosing option

--- employeeTracker.js
+++ employeeTracker.js
@@ -70,3 +70,24 @@ mainmenu = () => {
     });
 }
 
+viewAllEmployees = () => {
+    var query = "SELECT employee.id, employee.first_name, employee.last_name, roles.role_title, roles.salary from employee LEFT JOIN roles ON employee.role_id = roles.id";
+    connection.query(query, function (err, res) {
+      console.table(res);
+      mainmenu();
+    });
+}
+viewAllDept = () => {
+    var query = "SELECT department.id, department.dept_name from department";
+    connection.query(query, function (err, res) {
+      console.table(res);
+      mainmenu();
+    });
+}
+viewAllRoles = () => {
+    var query = "SELECT roles.id, roles.role_title, roles.salary, department.dept_name from roles LEFT JOIN department ON roles.dep_id = department.id";
+    connection.query(query, function (err, res) {
+      console.table(res);
+      mainmenu();
+    });
+}
